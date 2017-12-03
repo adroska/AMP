b1928c424ee495dc5244ec2472c2d6bc2a6f62dc
Adrienn Hodi <adri.hodi@gmail.com>
2017-12-03 20:15:53 +0100

    Add toolbox component, improve logo component, handle user interaction

--- amp-homework/src/app/app.module.ts
+++ amp-homework/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FooterComponent } from './pages/common/footer/footer.component';
@@ -9,6 +9,7 @@ import { LogoComponent } from './pages/common/logo/logo.component';
 import { CoursesComponent } from './pages/courses/courses/courses.component';
 import { CourseDetailsComponent } from './pages/courses/course-details/course-details.component';
 import { LoginComponent } from './pages/login/login/login.component';
+import { ToolBoxComponent } from './pages/courses/tool-box/tool-box.component';
 
 
 @NgModule({
@@ -19,10 +20,12 @@ import { LoginComponent } from './pages/login/login/login.component';
     LogoComponent,
     CoursesComponent,
     CourseDetailsComponent,
-    LoginComponent
+    LoginComponent,
+    ToolBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- amp-homework/src/app/pages/courses/courses/courses.component.ts
+++ amp-homework/src/app/pages/courses/courses/courses.component.ts
@@ -18,24 +18,26 @@ export class CoursesComponent implements OnInit {
             id: 1,
             title: 'Video course 1',
             duration: 88,
-            creationDate: new Date(),
+            creationDate: new Date(1992, 2, 10),
             description: 'Description of video course 1'
         },
         {
             id: 2,
             title: 'Video course 2',
             duration: 15,
-            creationDate: new Date(),
+            creationDate: new Date (2002, 3, 20),
             description: 'Description of video course 2'
         },
         {
             id: 3,
             title: 'Video course 3',
             duration: 135,
-            creationDate: new Date(),
+            creationDate: new Date(2012, 4, 30),
             description: 'Description of video course 3'
-        }
-    ];
-}
+        }];
+    }
 
+    deleteCourse(event) {
+        console.log(`${event.value}`);
+    }
 }
