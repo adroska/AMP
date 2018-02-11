4debbf2b3f0e9e387d2634aff2fb6f8060f82864
Adrienn Hodi <adri.hodi@gmail.com>
2018-02-12 00:33:18 +0100

    Add missing part

---
+++ amp-homework/src/app/app.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "@angular/router";
+import { CoursesComponent } from "./pages/courses/courses/courses.component";
+import { AuthGuard } from "./common/auth.guard";
+import { LoginComponent } from "./pages/login/login/login.component";
+import { AddCourseComponent } from "./pages/courses/add-course/add-course.component";
+import { NoContentComponent } from "./pages/no-content/no-content.component";
+
+export const ROUTES = [
+    { path: '',
+      redirectTo: 'courses',
+      pathMatch: 'full'
+    },
+    {
+      path: 'courses',
+      component: CoursesComponent,
+      canActivate: [AuthGuard]
+    },
+    {
+      path: 'login',
+      component: LoginComponent
+    },
+    {
+      path: 'add-course',
+      component: AddCourseComponent,
+      canActivate: [AuthGuard]
+    },
+    {
+        path: '**',
+        component: NoContentComponent
+    }
+  ];
